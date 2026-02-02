94f93a0450b1d5520c52ea258dcde3769cded516
daniel <danielbaradaran31-hash>
2026-02-02 20:24:32 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,20 @@
+import { useGSAP } from '@gsap/react'
 import { navLinks } from '../../constanc/index.js'
 
+
 const Navbar = () => {
+ useGSAP(() => {
+   const navTween = gsap.timeline({
+     scrollTrigger: {
+       trigger: 'nav',
+         start: 'bottom top'
+        }
+ });
+
+ navTween.fromTo('nav', { backgroundColor: 'transparent' }, {
+     backgroundColor: '#00000050', duration: 1,
+    ease: 'power1.inOut' });
+
   return (
     <nav>
         <div>
