a59e3d42fa957dfa953b4784f6f56aa4c4739672
daniel <danielbaradaran31-hash>
2026-02-05 22:47:58 +0100

    text

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,7 +1,33 @@
+import gsap from "gsap";
+import { useMediaQuery } from "react-responsive";
 import { featureLists, goodLists } from "../../constanc"
+import { useGSAP } from "@gsap/react";
 
 
 const Art = () => {
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
+    useGSAP(() => {
+        const start = isMobile ? 'top 20%' : 'top top';
+
+        const maskedTimeline = gsap.timeline({
+            scrollTrigger: {
+                trigger: '#art',
+                start,
+                end: 'bottom center',
+                scrub: 1.5,
+                pin: true,
+            }
+        })
+
+        maskedTimeline
+        .to('.will-fade', {
+            opacity: 0, stagger: 0.2, ease: 'power1.inOut',
+        })
+        .to('.masked-img', { scale: 1.3, maskPosition: 'center', maskSize: '400%',
+            duration: 1, ease: 'power1.inOut'})
+            .to('#masked-content', { opacity: 1, duration: 1, ease: 'power1.inOut' }, '-=0.5')
+    })           
   return (
     <div id='art'>
         <div className='container mx-auto h-full pt-1'>
