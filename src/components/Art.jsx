d82ad08422fd6b87bced6f6a970a1d1141b161bf
daniel <danielbaradaran31-hash>
2026-02-05 21:45:28 +0100

    img

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+
 
 const Art = () => {
   return (
-    <div>Art</div>
+    <div id='art'>
+        <div className='container mx-auto h-full pt-20'>
+            <h2 className='will-fade'>The ART</h2>
+
+            <div className='content'>
+                <ul className='space-y-4 will-fade'>
+                    {goodLists.map(( feature, index ) => (
+                        <li key={index} className='flex items-center gap-2'>
+                            <img src='/public/images/check.png' alt='check' />
+                            <p>{feature}</p>
+                        </li>
+                    ))}
+                </ul>
+                <div>
+                    <img 
+                    src="/public/images/under-img.jpg"
+                    alt="cocktail"
+                    className="abs-center masked-img size-full object-contain"
+                        />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
