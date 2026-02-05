0f7fd2f2b74bda23fecc8bfcc2311ecaa377a108
daniel <danielbaradaran31-hash>
2026-02-05 19:35:05 +0100

    add <p> and h2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div id='about'>
+        <div className='mb-16 md:px-0 px-5'>
+            <div className='content'>
+                <div className='md:col-span-8'>
+                    <p className='badge'>Best Cocktails</p>
+                    <h2>Where every detail matters <span className='text-white'>-</span>
+                    from muddle to garnish
+                    </h2>
+                </div>
+                < div className='sub-content'>
+
+                </div>
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
