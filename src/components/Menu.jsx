489a85b53e820d789d84f72babf96ea23f3e0583
daniel <danielbaradaran31-hash>
2026-02-07 01:16:15 +0100

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,7 +10,12 @@ const Menu = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
  useGSAP(() => {
-  gsap.from('#title', { opacity: 0 }, { opacity: 1, duration: 1 });
+  gsap.fromTo('#title', { opacity: 0 }, { opacity: 1, duration: 1 });
+  gsap.fromTo('.cocktail img', { opacity: 0, xPercent: -100 }, { xPercent: 0, opacity: 1,  duration: 1, ease: 'power1.inOut' 
+
+  })
+  gsap.fromTo('.details h2', {yPercent: 100, opacity: 0}, {yPercent: 0, opacity: 100, duration: 1, ease: 'power1.inOut'})
+  gsap.fromTo('.details p', {yPercent: 100, opacity: 0}, {yPercent: 0, opacity: 100, duration: 1, ease: 'power1.inOut'})
   }, [currentIndex]);
 
   const totalCocktails = allCocktails.length;
